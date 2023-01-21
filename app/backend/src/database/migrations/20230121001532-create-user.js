78d98e9b86772eeb0f0abcc125426a5105f0f4e2
Rafael Souza <souza_rafael.97@outlook.com>
2023-01-20 21:19:47 -0300

    Adiciona Model User

---
+++ app/backend/src/database/migrations/20230121001532-create-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+        email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    }, {
+      timestamps: false
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ app/backend/src/database/models/User.models.ts
@@ -0,0 +1,42 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
